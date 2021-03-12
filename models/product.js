25e39ebf4e04967776d3d66a3477d946fd73adc3
Theophilus Ighalo <theophilus.ighalo@venturegardengroup.com>
2021-03-12 16:41:40 +0100

    add memory models

--- models/product.js
+++ models/product.js
@@ -1,11 +1,37 @@
 const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
 
-const ProductSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
-    imageURL: { type: String, required: true },
-    // creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true })
+const products = []
+class Product {
+    constructor(obj) {
+        this.id = uuidv4() 
+        this.title = obj.title
+        this.price = obj.price
+        this.description = obj.description
+        this.imageURL = obj.imageURL
+        this.imageURL = obj.imageURL
+    }
 
-module.exports = Product = mongoose.model('Product', ProductSchema)
+    save() {
+        products.unshift(this)
+    }
+
+    static findAll() {
+        return products
+    }
+
+    static findById(id) {
+        return products.find(item => item.id === id)
+    }
+
+    static findByIdAndUpdate(id, obj) {
+        const index = products.findIndex(item => item.id === id)
+        products[index] = obj
+    }
+
+    static findByIdAndDelete(id) {
+        products = products.filter(item => item.id !== id)
+    }
+}
+
+module.exports = Product
--- models/user.js
+++ models/user.js
@@ -1,51 +1,77 @@
 const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
-    password: { type: String, required: true },
-    avatar: { type: String },
-    resetToken: String,
-    resetTokenExpiration: Date,
-    cart: {
-        items: [{
-            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-            quantity: { type: Number, require: true }
-        }]
+const users = []
+class User {
+    constructor(obj) {
+        this.id = uuidv4() 
+        this.username = obj.username
+        this.email = obj.email
+        this.password = obj.password
+        this.avatar = obj.avatar
+        this.imageURL = obj.imageURL
+        this.resetToken = null
+        this.resetTokenExpiration = null
+        this.cart = {
+            items: [{
+                product: '',
+                quantity: 0
+            }]
+        }
     }
-}, { timestamps: true })
 
-userSchema.methods.addToCart = function(product) {
-    const updatedCartItems = [...this.cart.items]
-    console.log()
-    const existingProductIndex = updatedCartItems.findIndex(item => {
-        console.log(item.product, product._id)
-        console.log(typeof item.product, typeof product._id)
-        return item.product.toString() === product._id.toString()
-    })
+    save() {
+        users.unshift(this)
+    }
 
-    if(existingProductIndex >= 0) { // Product already in cart, increment quantity
-        updatedCartItems[existingProductIndex].quantity += 1
-    } else { // Product not in cart, add to cart
-        updatedCartItems.push({ product: product._id, quantity: 1 })
+    static findAll() {
+        return users
+    }
+
+    static findById(id) {
+        return users.find(item => item.id === id)
+    }
+
+    static findByIdAndUpdate(id, obj) {
+        const index = users.findIndex(item => item.id === id)
+        users[index] = obj
+    }
+
+    static findByIdAndDelete(id) {
+        users = users.filter(item => item.id !== id)
+    }
+
+    static addToCart(product) {
+        const updatedCartItems = [...this.cart.items]
+        console.log()
+        const existingProductIndex = updatedCartItems.findIndex(item => {
+            console.log(item.product, product._id)
+            console.log(typeof item.product, typeof product._id)
+            return item.product.toString() === product._id.toString()
+        })
+    
+        if(existingProductIndex >= 0) { // Product already in cart, increment quantity
+            updatedCartItems[existingProductIndex].quantity += 1
+        } else { // Product not in cart, add to cart
+            updatedCartItems.push({ product: product._id, quantity: 1 })
+        }
+        
+        const updatedCart = {
+            items: updatedCartItems
+        }
+    
+        this.cart = updatedCart
+    
+        return this.save()
     }
     
-    const updatedCart = {
-        items: updatedCartItems
+    static removeFromCart(product) {
+        const updatedCartItems = this.cart.items.filter(item => item.product.toString() !== product._id.toString())
+    
+        this.cart.items = updatedCartItems
+    
+        return this.save()
     }
-
-    this.cart = updatedCart
-
-    return this.save()
 }
 
-userSchema.methods.removeFromCart = function(product) {
-    const updatedCartItems = this.cart.items.filter(item => item.product.toString() !== product._id.toString())
-
-    this.cart.items = updatedCartItems
-
-    return this.save()
-}
-
-
-module.exports = User = mongoose.model('User', userSchema)
+module.exports = User
