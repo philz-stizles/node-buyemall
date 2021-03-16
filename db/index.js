ee9fa81421d219dffd0602b1907f30e55d372082
philz-stizles <theophilusighalo@gmail.com>
2021-03-16 12:29:59 +0100

    Initialize sequelize

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+// const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_LOCAL_HOST,
+    dialect:'mysql'
+});
